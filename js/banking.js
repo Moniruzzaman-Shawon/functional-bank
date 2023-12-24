4735a791cdd0b7ffe3028e3148f7e2d5e39a687b
Moniruzzaman Shawon <m.zaman.djp@gmail.com>
2023-12-24 08:10:31 +0600

    validation created

--- js/banking.js
+++ js/banking.js
@@ -14,28 +14,51 @@ function updateTotalField(totalFieldId, amount){
     totalField.innerText =  amount + previuosTotal;
 }
 
-document.getElementById('btn-deposit').addEventListener('click',function(){
-   
-    const depositAmount = getInputValue('deposit-field'); // calling function
- 
-    updateTotalField('deposit-total', depositAmount);
+function getCurrentBalance(){
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
     const previuosBalanceTotal = parseFloat(balanceTotalText);
+    return previuosBalanceTotal;
+}
 
-    balanceTotal.innerText = previuosBalanceTotal + depositAmount;
+function updateBalance(amount, isAdd){
+    const balanceTotal = document.getElementById('balance-total');
+    const previuosBalanceTotal = getCurrentBalance();
+    if (isAdd == true){
+        balanceTotal.innerText = previuosBalanceTotal + amount;
+    }
+    else{
+        balanceTotal.innerText = previuosBalanceTotal - amount;
+    }
+}
+
+
+document.getElementById('btn-deposit').addEventListener('click',function(){
+   
+    const depositAmount = getInputValue('deposit-field'); // calling function
+    if(depositAmount > 0){
+        updateTotalField('deposit-total', depositAmount);
+        updateBalance(depositAmount, true);
+    }
+    else{
+        alert('Invalid Amount');
+    }
+
+
 });
 
 //withdraw functions
 document.getElementById('btn-withdraw').addEventListener('click',function(){
     const withdrawAmount = getInputValue('withdraw-field');
- 
-    
-    updateTotalField('withdraw-total', withdrawAmount);
-    //update total balance after withdraw
-    const balanceTotal = document.getElementById('balance-total');
-    const balanceTotalText = balanceTotal.innerText;
-    const previuosBalanceTotal = parseFloat(balanceTotalText);
-
-    balanceTotal.innerText = previuosBalanceTotal - withdrawAmount;
+    const currentBalance = getCurrentBalance();
+    if(withdrawAmount > 0 && withdrawAmount < currentBalance){
+        updateTotalField('withdraw-total', withdrawAmount);
+        updateBalance(withdrawAmount, false);  
+    }
+    else if(withdrawAmount > currentBalance){
+        alert('You do not have enough money');
+    }
+    else{
+        alert('Invalid Amount');
+    }
 });
